f9da5165a0a2d5cf0b2c0c9f7b026096b6a10002
JoshuaDull <joshualdull@gmail.com>
2015-12-01 12:26:39 -0500

    Coordinates added through geonames

--- geonamesCode.py
+++ geonamesCode.py
@@ -12,26 +12,27 @@ with open("birthGeo.csv", "r") as in_file, open("birthGeonames.csv", "w") as out
     row0.append('longitude')
     row0.append('latitude')
     row0.append('geonameId')
-    print(row0)
+#    print(row0)
     for row in x:
       name = row[2]
       city = row[6]
       #state = row[7]
       country = row[8]
       location = city +" "+ country
-      print(location)
+#      print(location)
       payload = { 'q' : location, 'maxRows' : 1, 'username' : "joshuadull"}
       r = requests.get("http://api.geonames.org/searchJSON", params = payload)
-      print(r.url)
+#      print(r.url)
       data = json.loads(r.text)
       try:
         row.append(data['geonames'][0]['lng'])
         row.append(data['geonames'][0]['lat'])
         row.append(data['geonames'][0]['geonameId'])
         w.writerow(row)
-        print(row)
+#        print(row)
       except:
-        print(name + city + " was not found")
+#        w.write(row)
+        print(row)
 # http://api.geonames.org/searchJSON?q=warsaw%20poland&maxRows=1&username=joshuadull
 
 with open("deathGeo.csv", "r") as in_file, open("deathGeonames.csv", "w") as out_file:
@@ -41,23 +42,24 @@ with open("deathGeo.csv", "r") as in_file, open("deathGeonames.csv", "w") as out
     row0.append('longitude')
     row0.append('latitude')
     row0.append('geonameId')
-    print(row0)
+ #   print(row0)
     for row in x:
       name = row[3]
       city = row[6]
       #state = row[7]
       country = row[8]
       location = city +" "+ country
-      print(location)
+ #     print(location)
       payload = { 'q' : location, 'maxRows' : 1, 'username' : "joshuadull"}
       r = requests.get("http://api.geonames.org/searchJSON", params = payload)
-      print(r.url)
+ #     print(r.url)
       data = json.loads(r.text)
       try:
         row.append(data['geonames'][0]['lng'])
         row.append(data['geonames'][0]['lat'])
         row.append(data['geonames'][0]['geonameId'])
         w.writerow(row)
-        print(row)
+ #       print(row)
       except:
-        print(name + city + " was not found")
+ #       w.write(row)
+        print(row)
