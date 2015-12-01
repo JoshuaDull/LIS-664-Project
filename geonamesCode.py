893081f3906d5efeac8f05b2416ff1278dbb0cd6
JoshuaDull <joshualdull@gmail.com>
2015-12-01 01:57:05 -0500

    All with Lat, Long, and geonameId

--- geonamesCode.py
+++ geonamesCode.py
@@ -14,7 +14,7 @@ with open("birthGeo.csv", "r") as in_file, open("birthGeonames.csv", "w") as out
     row0.append('geonameId')
     print(row0)
     for row in x:
-      name = row[3]
+      name = row[2]
       city = row[6]
       #state = row[7]
       country = row[8]
@@ -25,9 +25,9 @@ with open("birthGeo.csv", "r") as in_file, open("birthGeonames.csv", "w") as out
       print(r.url)
       data = json.loads(r.text)
       try:
-        row.append(data['geonamesbirth'][0]['lng'])
-        row.append(data['geonamesbirth'][0]['lat'])
-        row.append(data['geonamesbirth'][0]['geonameId'])
+        row.append(data['geonames'][0]['lng'])
+        row.append(data['geonames'][0]['lat'])
+        row.append(data['geonames'][0]['geonameId'])
         w.writerow(row)
         print(row)
       except:
@@ -54,9 +54,9 @@ with open("deathGeo.csv", "r") as in_file, open("deathGeonames.csv", "w") as out
       print(r.url)
       data = json.loads(r.text)
       try:
-        row.append(data['geonamesdeath'][0]['lng'])
-        row.append(data['geonamesdeath'][0]['lat'])
-        row.append(data['geonamesdeath'][0]['geonameId'])
+        row.append(data['geonames'][0]['lng'])
+        row.append(data['geonames'][0]['lat'])
+        row.append(data['geonames'][0]['geonameId'])
         w.writerow(row)
         print(row)
       except:
