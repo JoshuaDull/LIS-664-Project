003e25f5265098aa26798ab01fa3d73f8b3ecfda
JoshuaDull <joshualdull@gmail.com>
2015-12-07 10:16:22 -0500

    Fixed API search issues

--- geonamesCode.py
+++ geonamesCode.py
@@ -12,26 +12,33 @@ with open("birthGeo.csv", "r") as in_file, open("birthGeonames.csv", "w") as out
     row0.append('longitude')
     row0.append('latitude')
     row0.append('geonameId')
+    row0.append('WhitneyUrl') 
+    w.writerow(row0)
 #    print(row0)
     for row in x:
       name = row[2]
       city = row[6]
-      #state = row[7]
+      state = row[7]
       country = row[8]
-      location = city +" "+ country
+      artistID = row[11]
+      location = city +" "+ state + " " + country
 #      print(location)
       payload = { 'q' : location, 'maxRows' : 1, 'username' : "joshuadull"}
       r = requests.get("http://api.geonames.org/searchJSON", params = payload)
 #      print(r.url)
       data = json.loads(r.text)
+      whitneyURL = '<a href="http://collection.whitney.org/artist/' + artistID + '">Whitney.org</a>'
+#      print(whitneyURL)
       try:
         row.append(data['geonames'][0]['lng'])
         row.append(data['geonames'][0]['lat'])
         row.append(data['geonames'][0]['geonameId'])
+        row.append(whitneyURL)
         w.writerow(row)
 #        print(row)
       except:
-#        w.write(row)
+        row.append(whitneyURL)
+        w.writerow(row)
         print(row)
 # http://api.geonames.org/searchJSON?q=warsaw%20poland&maxRows=1&username=joshuadull
 
@@ -42,24 +49,30 @@ with open("deathGeo.csv", "r") as in_file, open("deathGeonames.csv", "w") as out
     row0.append('longitude')
     row0.append('latitude')
     row0.append('geonameId')
+    row0.append('WhitneyURL')
+    w.writerow(row0)
  #   print(row0)
     for row in x:
       name = row[3]
       city = row[6]
-      #state = row[7]
+      state = row[7]
       country = row[8]
-      location = city +" "+ country
+      artistID = row[11]
+      location = city +" "+ state + " " + country
  #     print(location)
       payload = { 'q' : location, 'maxRows' : 1, 'username' : "joshuadull"}
       r = requests.get("http://api.geonames.org/searchJSON", params = payload)
  #     print(r.url)
+      whitneyURL = '<a href="http://collection.whitney.org/artist/' + artistID + '">Whitney.org</a>'
       data = json.loads(r.text)
       try:
         row.append(data['geonames'][0]['lng'])
         row.append(data['geonames'][0]['lat'])
         row.append(data['geonames'][0]['geonameId'])
+        row.append(whitneyURL)
         w.writerow(row)
  #       print(row)
       except:
- #       w.write(row)
+        row.append(whitneyURL) 
+        w.writerow(row)
         print(row)
